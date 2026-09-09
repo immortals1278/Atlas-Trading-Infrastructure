1081c2b7f61489e8095e413a3732021a09ecae41
immortals1278 <3156160490@qq.com>
2026-09-09 21:42:29 +0800

    完成kafka producer publish() 实现

--- internal/infrastructure/kafka/producer.go
+++ internal/infrastructure/kafka/producer.go
@@ -1,6 +1,9 @@
 package kafka
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/twmb/franz-go/pkg/kgo"
@@ -9,4 +12,27 @@ import (
 type Producer struct {
 	client         kgo.Client
 	publishTimeout time.Duration
+}
+
+func (p *Producer) Publish(ctx context.Context, topic, key string, payload interface{}) error {
+	PubCtx, cancel := context.WithTimeout(context.Background(), p.publishTimeout)
+	defer cancel()
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("序列化事件失败(topic: %s): %w", topic, err)
+	}
+
+	record := &kgo.Record{
+		Topic: topic,
+		Key:   []byte(key),
+		Value: data,
+	}
+
+	if err := p.client.ProduceSync(PubCtx, record).FirstErr(); err != nil {
+		return fmt.Errorf("发送事件失败(: %s): %w", topic, err)
+	}
+
+	return nil
+
 }
